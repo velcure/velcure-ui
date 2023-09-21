128cf736ba65f3d1058e3811bb75de35bd6b94ce
Julian Koehn <me@julian.pro>
2023-09-21 16:42:23 +0200

    feat: useFocusEffect

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -4,6 +4,7 @@ export * from './use-boolean';
 export * from './use-callback-ref';
 export * from './use-disclosure';
 export * from './use-event-listener';
+export * from './use-focus-effect';
 export * from './use-focus-on-pointer-down';
 export * from './use-update-effect';
 export * from './useDebounce';
