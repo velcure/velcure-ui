a88373bed1941f9ad9fe5450639a235245d5d9d3
Julian Koehn <me@julian.pro>
2023-09-22 13:21:54 +0200

    fix: storybook theme

---
+++ .storybook/theme.ts
@@ -0,0 +1,11 @@
+// .storybook/YourTheme.js
+
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Velcure',
+  brandUrl: 'https://velcure.com',
+  // brandImage: 'https://storybook.js.org/images/placeholders/350x150.png',
+  brandTarget: '_self',
+});
