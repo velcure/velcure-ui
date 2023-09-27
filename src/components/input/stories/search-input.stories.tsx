c3a9dad5636469e5363a9028bcc7d6c46b4e9243
Julian Koehn <me@julian.pro>
2023-09-27 19:25:11 +0200

    fix: story for search input

---
+++ src/components/input/stories/search-input.stories.tsx
@@ -0,0 +1,32 @@
+import { Meta } from '@storybook/react';
+import React from 'react';
+import { SearchInput } from '../src';
+
+const meta = {
+  title: 'Components / Forms / Search Input',
+  component: SearchInput,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof SearchInput>;
+
+export const searchInput = () => {
+  const [value, setValue] = React.useState('');
+  return (
+    <div>
+      <SearchInput value={value} onChange={(e) => setValue(e.target.value)} />
+
+      <button
+        onClick={() => {
+          setValue('test');
+        }}
+      >
+        Set value
+      </button>
+      <div>
+        <pre>{JSON.stringify(value, null, 2)}</pre>
+      </div>
+    </div>
+  );
+};
