a464d0be3a24484c63a156bffe80d097726fa2d3
Julian Koehn <me@julian.pro>
2023-09-27 10:43:58 +0200

    fix: export of property label

--- src/components/property/src/index.ts
+++ src/components/property/src/index.ts
@@ -4,3 +4,5 @@ export { PropertyLabel } from './property-label';
 export type { PropertyLabelProps } from './property-label';
 export { PropertyList } from './property-list';
 export type { PropertyListProps } from './property-list';
+export { PropertyValue } from './property-value';
+export type { PropertyValueProps } from './property-value';
